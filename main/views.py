6a0427c16c3b86eb52feeeb3516c636d439d4f67
Fazliddin Mamadaliyev <mamadaliyevfazliddin446@gmail.com>
2026-02-24 16:06:48 +0500

    fix duplicate urlpatterns

--- main/views.py
+++ main/views.py
@@ -1,16 +1,6 @@
-from django.shortcuts import render
-from .models import Profile, Project, Skill
+from django.urls import path
+from .views import home
 
-def home(request):
-    profile = Profile.objects.first()
-    projects = Project.objects.all()
-    skills = Skill.objects.all().order_by("-level")
-    return render(request, "home.html", {   # ✅ shu yer o'zgardi
-        "profile": profile,
-        "projects": projects,
-        "skills": skills
-    })
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'home.html')
+urlpatterns = [
+    path("", home, name="home"),
+]
