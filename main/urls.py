03e4b4b0d1068cc288f765d19e83cacc848b2834
Fazliddin Mamadaliyev <mamadaliyevfazliddin446@gmail.com>
2026-02-24 17:04:52 +0500

    make multipage modern layout

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from . import views
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", views.home, name="home"),
+    path("projects/", views.projects, name="projects"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,10 +3,23 @@ from .models import Profile, Project, Skill
 
 def home(request):
     profile = Profile.objects.first()
-    projects = Project.objects.all()
+    projects = Project.objects.all()[:3]   # home’da 3 tasini ko‘rsatamiz
     skills = Skill.objects.all().order_by("-level")
     return render(request, "home.html", {
         "profile": profile,
         "projects": projects,
         "skills": skills
     })
+
+def projects(request):
+    profile = Profile.objects.first()
+    projects = Project.objects.all()
+    return render(request, "projects.html", {"profile": profile, "projects": projects})
+
+def about(request):
+    profile = Profile.objects.first()
+    return render(request, "about.html", {"profile": profile})
+
+def contact(request):
+    profile = Profile.objects.first()
+    return render(request, "contact.html", {"profile": profile})
